7293187be4e186ac1d9a0895ffc1907d9cb73105
nelak <nelak@e107.com.ar>
2015-08-02 19:46:43 -0300

    Initial commit

---
+++ src/FSharp.Graphics.OpenGL/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("FSharp.Graphics.OpenGL")>]
+[<assembly: AssemblyProductAttribute("FSharp.Graphics.OpenGL")>]
+[<assembly: AssemblyDescriptionAttribute("F# OpenGL bindings")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
---
+++ tests/FSharp.Graphics.OpenGL.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module FSharp.Graphics.OpenGL.Tests
+
+open FSharp.Graphics.OpenGL
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
